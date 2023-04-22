f9be484bf70a5f5c83e86637bae2e1d3f2e19d0b
Pietra <pievalentinah@gmail.com>
2023-04-22 20:52:25 -0300

    first-commit-in-progress

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,19 @@
+const Search = () => {
+  return (
+    <div
+      className="
+            border-[1px]
+            w-full
+            md:w-auto
+            py-2
+            rounded-full
+            shadow-sm
+            hover:shadow-md
+            transition
+            cursor-pointer         
+         "
+    ></div>
+  );
+};
+
+export default Search;
