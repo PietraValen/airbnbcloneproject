cf1bd3a740927ba2bcf932ab0f009a1e2058e47f
Pietra <pievalentinah@gmail.com>
2023-05-18 23:49:56 -0300

    primeira-parte-navbar-ok

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use cliet";
+
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <Image
+      className="rounded-full"
+      height="30"
+      width="30"
+      alt="Avatar"
+      src="/Images/placeholder.jpg"
+    />
+  );
+};
+
+export default Avatar;
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { AiOutlineMenu } from "react-icons/ai";
+import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value);
+  }, []);
+
   return (
     <div className="relative">
       <div
@@ -31,7 +40,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
+          onClick={toggleOpen}
           className="
                 p-4
                 md:py-1
@@ -49,8 +58,40 @@ const UserMenu = () => {
             "
         >
           <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
       </div>
+
+      {isOpen && (
+        <div
+          className="
+            absolute
+            rounded-x1
+            shadow-md
+            w-[40vw]
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+          "
+        >
+          <div className="flex flex-col cursor-pointer">
+            <>
+              <MenuItem
+               onClick={() => {}}
+               label="Login"
+              />
+              <MenuItem
+               onClick={() => {}}
+               label="Sign up"
+              />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
